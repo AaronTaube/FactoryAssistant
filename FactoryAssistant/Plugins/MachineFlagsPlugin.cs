8164c8faebb0a92561f32c73fe76b0f8dfe82ee9
Aaron Taube <aaron.taube@gmail.com>
2025-07-20 14:04:27 -0500

    init

---
+++ FactoryAssistant/Plugins/MachineFlagsPlugin.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.SemanticKernel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+public class MachineFlagsPlugin
+{
+	// Mock data for the lights
+	private readonly List<MachineModel> machines = new()
+   {
+	  new MachineModel { Id = 1, Name = "Service Elevator", RepairNeeded = false, InspectionDueDate = DateTime.Today },
+	  new MachineModel { Id = 2, Name = "Lift A", RepairNeeded = false, InspectionDueDate = DateTime.Today },
+	  new MachineModel { Id = 3, Name = "Lift B", RepairNeeded = true, InspectionDueDate = DateTime.Today },
+	  new MachineModel { Id = 4, Name = "Torch", RepairNeeded = true, InspectionDueDate = DateTime.Today }
+   };
+
+	[KernelFunction("get_machines")]
+	[Description("Gets a list of machines and their current state")]
+	public async Task<List<MachineModel>> GetMachinesAsync()
+	{
+		return machines;
+	}
+
+	[KernelFunction("update_inspection")]
+	[Description("Changes the inspection due date.")]
+	public async Task<MachineModel?> UpdateInspectionDateAsync(int id)
+	{
+		var machine = machines.FirstOrDefault(light => light.Id == id);
+
+		if (machine == null)
+		{
+			return null;
+		}
+
+		// Update the light with the new state
+		machine.InspectionDueDate = DateTime.Today.AddMonths(3);
+
+		return machine;
+	}
+	public class MachineModel
+	{
+		[JsonPropertyName("id")]
+		public int Id { get; set; }
+
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("repair_needed")]
+		public bool? RepairNeeded { get; set; }
+		[JsonPropertyName("inspection_due_date")]
+		public DateTime? InspectionDueDate { get; set; }
+
+	}
+}
--- FactoryAssistant/Program.cs
+++ FactoryAssistant/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Import packages
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+var configBuilder = new ConfigurationBuilder()
+	.SetBasePath(Directory.GetCurrentDirectory())
+	.AddJsonFile("appsettings.json")
+	.Build();
+
+// Populate values from your OpenAI deployment
+var modelId = configBuilder.GetSection("Settings:model-id").Value;
+var endpoint = configBuilder.GetSection("Settings:ai-endpoint").Value;
+var apiKey = configBuilder.GetSection("Settings:ai-key").Value;
+
+
+// Create a kernel with Azure OpenAI chat completion
+var kernelBuilder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+// Add enterprise components
+kernelBuilder.Services.AddLogging(services => services.SetMinimumLevel(LogLevel.Trace));
+
+// Build the kernel
+Kernel kernel = kernelBuilder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Add a plugin (the LightsPlugin class is defined below)
+kernel.Plugins.AddFromType<LightsPlugin>("Lights");
+kernel.Plugins.AddFromType<MachineFlagsPlugin>("Machines");
+
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new()
+{
+	FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+// Initiate a back-and-forth chat
+string? userInput;
+do
+{
+	// Collect user input
+	Console.Write("User > ");
+	userInput = Console.ReadLine();
+
+	// Add user input
+	history.AddUserMessage(userInput);
+
+	// Get the response from the AI
+	var result = await chatCompletionService.GetChatMessageContentAsync(
+		history,
+		executionSettings: openAIPromptExecutionSettings,
+		kernel: kernel);
+
+	// Print the results
+	Console.WriteLine("Assistant > " + result);
+
+	// Add the message from the agent to the chat history
+	history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
